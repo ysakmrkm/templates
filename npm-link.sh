45ecc2f57950b1db47c2dd2fb6a3431c22c47cf6
ysakmrkm <info@ysakmrkm.com>
2014-07-04 16:28:29 +0900

    Add npm install -g

--- npm-link.sh
+++ npm-link.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-for i in `cat package.json | jq '.devDependencies | keys | .[]'`;\
-do `echo npm link $i | sed -e 's/"//g'`;done
+for i in `cat package.json | jq '.devDependencies | keys | .[]'`;
+do
+`echo npm install -g $i| sed -e 's/"//g'`;
+`echo npm link $i | sed -e 's/"//g'`;
+done
