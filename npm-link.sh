fe26ca64d5ca978e48fc367f39db2929cae61ef8
ysakmrkm <info@ysakmrkm.com>
2016-01-26 11:25:56 +0900

    Add argument for new package installation

--- npm-link.sh
+++ npm-link.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]; then
+
 for i in `cat package.json | jq '.devDependencies | keys | .[]'`;
 do
 `echo npm link $i | sed -e 's/"//g'`;
 done
+
+elif [ $# -eq 1 ]; then
+
+`echo npm install $1 --save-dev`;
+`echo npm install -g $1`;
+`echo npm uninstall $1`;
+`echo npm link $1`;
+
+else
+
+exit 1
+
+fi
