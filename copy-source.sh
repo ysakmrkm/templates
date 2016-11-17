b02f87f07ba165ae7a6350de67f402f72d60ba1a
ysakmrkm <info@ysakmrkm.com>
2016-11-17 12:24:57 +0900

    Add script that copy source files

---
+++ copy-source.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "Need path that files are exist.";
+
+  exit 1
+else
+  `echo mkdir -p ./src`;
+  `echo cp -r ${1%/}/jade ./src`;
+  `echo cp -r ${1%/}/sass ./src`;
+  `echo cp -r ${1%/}/cs ./src`;
+
+  echo "Finish copy source files.";
+fi
