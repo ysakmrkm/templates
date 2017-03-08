dff2343c27947a6a35b72544106aad43da523d1b
ysakmrkm <info@ysakmrkm.com>
2017-03-08 19:13:01 +0900

    Add .stylelintrc to init-config

--- init-config.sh
+++ init-config.sh
@@ -8,6 +8,7 @@ else
   `echo ln -fs ${1%/}/.editorconfig ~/.editorconfig`;
   `echo ln -fs ${1%/}/.pug-lintrc ./.pug-lintrc`;
   `echo ln -fs ${1%/}/coffeelint.json ./coffeelint.json`;
+  `echo ln -fs ${1%/}/.stylelintrc ~/.stylelintrc`;
   `echo ln -fs ${1%/}/.scss-lint.yml ~/.scss-lint.yml`;
   `echo ln -fs ${1%/}/package_gulp.json ./package.json`;
   `echo cp -f ${1%/}/config.rb ./config.rb`;
